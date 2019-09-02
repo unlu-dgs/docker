c3304362e69e93f5b1a13ab1ec1c78aeff6d7376
Andrés Blanco <ablanco@siu.edu.ar>
2019-09-02 14:13:57 -0300

    fixes para rancher

--- kubectl/1.15/sync.sh
+++ kubectl/1.15/sync.sh
@@ -17,7 +17,7 @@ function get_configmap_name () {
 }
 
 function format_status () {
-    echo -e "\e[34m> $1"
+    echo -e "\e[33m> $1"
     echo -e "\e[0m"
 }
 
@@ -29,10 +29,14 @@ function format_cmd_output () {
 
 # https://github.com/kubernetes/kubernetes/issues/72794#issuecomment-483502617
 function get_deploy_pods_for () {
-    local selfLink=$(kubectl get deployment.apps "${1}" -n "${k8s_ns}" -o jsonpath={.metadata.selfLink})
-    local selector=$(kubectl get --raw "${selfLink}"/scale | jq -r .status.selector)
-    local pods=$(kubectl get pods -n "${k8s_ns}" --field-selector=status.phase==Running -o=name --selector "${selector}" | sed "s/^.\{4\}//")
-    echo "${pods}"
+    #local selfLink=$(kubectl get deployment.apps "${1}" -n "${k8s_ns}" -o jsonpath={.metadata.selfLink})
+    #format_status ${selfLink}
+    #local selector=$(kubectl get --raw "${selfLink}"/scale | jq -r .status.selector)
+    #format_status ${selector}
+    #local pods=$(kubectl get pods -n "${k8s_ns}" --field-selector=status.phase==Running -o=name --selector "${selector}" | sed "s/^.\{4\}//")
+
+    local pods=$(kubectl get pods -n "${k8s_ns}" --selector=app="${1}" -o custom-columns=":metadata.name")
+    echo "${pods}" 
 }
 
 function get_first_deploy_pod_for () {
@@ -48,6 +52,7 @@ function save_configmap () {
     format_status "Actualizando configmap ${configmap} para ${1}"
     kubectl exec ${pod} -- sh -c "cat arai.lock" > /tmp/arai.lock
     kubectl create configmap ${configmap} --from-file=/tmp/arai.lock --dry-run -o yaml | kubectl apply -f - 
+    format_status "Configmap ${configmap} actualizado para ${1}"
     rm /tmp/arai.lock
 }
 
@@ -61,7 +66,10 @@ function cargar_configmaps () {
         format_status "Cargando configmap en ${pod}"
         configmap="$(get_configmap_name ${registry_id})"
         kubectl get configmap ${configmap} -o jsonpath='{.data.arai\.lock}' > /tmp/arai.lock
-        kubectl cp /tmp/arai.lock ${pod}:/usr/local/app/arai.lock
+        local pod_path="${k8s_ns}"/"${pod}"
+        format_status "Copiando arai.lock en ${pod_path}"
+        kubectl cp -n ${k8s_ns} /tmp/arai.lock ${pod_path}:/usr/local/app/arai.lock
+        format_status "Configmap cargado en ${pod}"
         rm /tmp/arai.lock
     done
 }
